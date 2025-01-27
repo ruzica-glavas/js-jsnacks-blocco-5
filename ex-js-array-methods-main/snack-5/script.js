19abf105fe9cc1bb63ee13e4a0ecb0d8029ee621
ruzica-glavas <ruzi99@hotmail.it>
2025-01-27 12:28:31 +0100

    add: snack 5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -2,3 +2,11 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const result = nums.filter(checkPari);
+
+function checkPari (nums){
+    return nums%2===0;
+}
+
+console.log(result)
