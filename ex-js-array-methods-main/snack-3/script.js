951be928199de25158882d904f35f4d24f5caeca
ruzica-glavas <ruzi99@hotmail.it>
2025-01-27 12:19:05 +0100

    add: snack 3

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -2,3 +2,7 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const newArrayIncrease= numbers.map(element => element+1)
+
+console.log(newArrayIncrease)
