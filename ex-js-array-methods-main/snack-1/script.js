55c24cd6fe5a7a3b230f76746462e114b9770999
ruzica-glavas <ruzi99@hotmail.it>
2025-01-27 12:04:39 +0100

    first commit

---
+++ ex-js-array-methods-main/snack-1/script.js
@@ -0,0 +1,8 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+console.log(names.join(","))
+
+//Usato il join per convertire l'array in una stringa
